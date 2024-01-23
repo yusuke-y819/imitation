cf395b41fb882f6ef5b5ce0bc419c1db1de4a729
yusuke-y819 <abc@example.com>
2024-01-24 05:49:55 +0900

    add: add environment for RC car racing

---
+++ racing/MyEnv/config.py
@@ -0,0 +1,12 @@
+# env.pyで使用する設定ファイル
+config = {
+    # actionの最大値、最小値
+    'steer_max' : 1.0,
+    'steer_min' : -1.0,
+    'throttle_max' : 1.0,
+    'throttle_min' : 0.0,
+
+    # 画像のサイズ
+    'width' : 224,
+    'height' : 224,
+}
